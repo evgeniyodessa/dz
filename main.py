0e401845eb3210e204b6c828dc5d4fcdc56de139
evgenyodessa <evheniistoianov@gmail.com>
2022-09-16 23:34:55 +0300

    dz5.1f

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-a=int(input("vvod a"))
-b=int(input("vvod b"))
-c=int(input("vvod c"))
+a=int(input("vvod a: "))
+b=int(input("vvod b: "))
+c=int(input("vvod c: "))
 d=a%3
 e=b%3
 if a>10 and b>10 and c>10 and d==0 and e==0:
