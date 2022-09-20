00e06a81652eaa0422b385f00d9fc2dac24c938a
evgenyodessa <evheniistoianov@gmail.com>
2022-09-20 20:38:23 +0300

    dz5.1f

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 a=int(input("vvod a: "))
 b=int(input("vvod b: "))
 c=int(input("vvod c: "))
-d=a%3
-e=b%3
-if a>10 and b>10 and c>10 and d==0 and e==0:
+if a>10 and b>10 and c>10 and a%3==0 and b%3==0:
     print("yes")
 else:
     print("no")
