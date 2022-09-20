1ae67b0dca6c75f147544d76065c2a27af76c52f
evgenyodessa <evheniistoianov@gmail.com>
2022-09-21 00:27:14 +0300

    dz5.1f

---
+++ dz6.py
@@ -0,0 +1,97 @@
+# задание 1
+N=int(input("введите ширину: "))
+while N!=0:
+    print("*"*N)
+    N-=1
+
+# задание 2
+N=int(input("введите ширину: "))
+a=1
+while a<=N:
+    print("*"*a)
+    a+=1
+
+# задание 3
+N=int(input("введите ширину: "))
+a=0
+while N!=0:
+    print(a*" "+"*"*N)
+    N-=1
+    a+=1
+
+# задание 4
+N=int(input("введите ширину: "))
+a=1
+while N!=0:
+    print((N-1)*" "+"*"*a)
+    N-=1
+    a+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+1) Запросить у пользователя число N, вывести все числа от 0 до N которые делятся на 3.
+"""
+
+
+# N = int(input('Enter number: '))
+# number = 0
+
+# while number < N:
+#
+#     if number % 3 == 0:
+#         print(number)
+#
+#     number += 1
+
+
+# while number < N:
+#     print(number)
+#     number += 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
