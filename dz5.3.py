5d3fe8784d3b47d55d005be19780a27aa4bf3b63
evgenyodessa <evheniistoianov@gmail.com>
2022-09-20 20:37:42 +0300

    dz5.1f

--- dz5.3.py
+++ dz5.3.py
@@ -2,8 +2,10 @@ number=int(input("vvod: "))
 a=int(number/100)
 b=int((number/10)%10)
 c=int(number%10)
-reversed_number=(str(c)+str(b)+str(a))
-print(reversed_number)
+reversed_number=(c*100+b*10+a)
+print(f'{reversed_number:03}')
+
+
 
 
 
