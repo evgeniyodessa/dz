c699742cb50ec0aa6b90394ce40c0955d71c56ba
evgenyodessa <evheniistoianov@gmail.com>
2022-09-21 19:34:50 +0300

    dz5.1f

--- dz6.py
+++ dz6.py
@@ -1,28 +1,28 @@
 # задание 1
-N=int(input("введите ширину: "))
-while N!=0:
-    print("*"*N)
-    N-=1
+N = int(input("введите ширину: "))
+while N != 0:
+    print("*" * N)
+    N -= 1
 
 # задание 2
-N=int(input("введите ширину: "))
-a=1
-while a<=N:
-    print("*"*a)
-    a+=1
+N = int(input("введите ширину: "))
+a = 1
+while a <= N:
+    print("*" * a)
+    a += 1
 
 # задание 3
-N=int(input("введите ширину: "))
-a=0
-while N!=0:
-    print(a*" "+"*"*N)
-    N-=1
-    a+=1
+N = int(input("введите ширину: "))
+a = 0
+while N != 0:
+    print(a * " " + "*" * N)
+    N -= 1
+    a += 1
 
 # задание 4
-N=int(input("введите ширину: "))
-a=1
-while N!=0:
-    print((N-1)*" "+"*"*a)
-    N-=1
-    a+=1
+N = int(input("введите ширину: "))
+a = 1
+while N != 0:
+    print((N - 1) * " " + "*" * a)
+    N -= 1
+    a += 1
